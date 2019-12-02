98c3a131428d3c9d0005972bcfa4eaea3771a1c5
Nick Ng <ngyewch@gmail.com>
2019-12-02 11:51:49 +0800

    fix: Reordered plugin setup.

--- src/main/java/org/arl/fjage/gradle/FjagePlugin.java
+++ src/main/java/org/arl/fjage/gradle/FjagePlugin.java
@@ -13,13 +13,14 @@ public class FjagePlugin implements Plugin<Project> {
         project.getTasks().register("packageFjage", FjagePackagingTask.class,
                 task -> task.dependsOn("jar"));
 
+        final Configuration testImplementationConfiguration = project.getConfigurations()
+                .getByName("testImplementation");
+        testImplementationConfiguration.extendsFrom(fjageConfiguration);
+
         project.afterEvaluate(p -> {
             final Configuration compileClasspathConfiguration = project.getConfigurations()
                     .getByName("compileClasspath");
-            final Configuration testImplementationConfiguration = project.getConfigurations()
-                    .getByName("testImplementation");
             compileClasspathConfiguration.extendsFrom(fjageConfiguration);
-            testImplementationConfiguration.extendsFrom(fjageConfiguration);
 
             project.getTasks().getByName("assemble")
                     .dependsOn("packageFjage");
